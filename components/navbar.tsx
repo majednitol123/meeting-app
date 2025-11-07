8399aaad7149b66c55651698761f491ad8d327e9
majednitol <majadrahman7@gmail.com>
2025-11-07 18:06:42 +0600

    done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
-import { links } from "@/config";
+
 
 import { MobileNav } from "./mobile-nav";
 
